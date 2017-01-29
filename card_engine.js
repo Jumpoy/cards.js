caa7fd906076bd14bb2eff868fbdec1ab4ca0fea
Nick <nicholas@hiebl.net.au>
2017-01-29 16:50:49 +1100

    Added original files

---
+++ card_engine.js
@@ -0,0 +1,49 @@
+var canvas,
+    ctx,
+    currentScreen;
+
+var width,
+    height;
+
+var update,
+    draw,
+    setup;
+
+var mousePressed;
+
+var MOUSE = {
+    x: 0,
+    y: 0,
+}
+
+var PI = Math.PI;
+
+function beginLoop() {
+
+    var frameId = 0;
+    var lastFrame = Date.now();
+
+    function loop() {
+        var thisFrame = Date.now();
+
+        var elapsed = thisFrame - lastFrame;
+
+        frameId = window.requestAnimationFrame(loop);
+
+        if (update !== undefined) {
+            update(elapsed);
+        }
+
+        if (draw !== undefined) {
+            draw();
+        }
+
+        lastFrame = thisFrame;
+    }
+
+    loop();
+}
+
+window.onload = function() {
+    
+}
